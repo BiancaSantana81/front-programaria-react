57eb604d2e553dd6888b4109da782e00e26f05f3
bsantana <biancasantana81@hotmail.com>
2024-10-25 13:41:02 -0300

    update front end

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,11 @@ import styles from '../styles/footer.module.css'
 export function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <p className={styles.footerText}> Front-end feito com ♡ por Simara Conceição para PrograMaria.</p>
-      <p className={styles.footerText}> Back-end desenvolvido por Bianca Santana para projeto de curso. ♡ </p>
+      <p className={styles.footerText}>
+        Front-end feito com ♡ por Simara Conceição para PrograMaria.
+        <br />
+        Back-end desenvolvido por Bianca Santana para projeto de curso. ♡
+      </p>
     </div>
   )
 }
